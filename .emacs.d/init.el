c78c464ebdbe64c330c5028663787ac9acdb7c58
benevolent <mikio.fjt@gmail.com>
2014-08-25 14:53:23 +0900

    initial commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,266 @@
+; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;; ------------------------------------------------------------------------
+;; @ load-path
+
+;; load-pathの追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; load-pathに追加するフォルダ
+;; 2つ以上フォルダを指定する場合の引数 => (add-to-load-path "elisp" "xxx" "xxx")
+(add-to-load-path "elisp" "conf" "public_repo")
+
+;; ------------------------------------------------------------------------
+;; @ general
+
+;; common lisp
+(require 'cl)
+
+;;c-x c-fのデフォルト
+(setq default-directory "~/")
+
+;; 文字コード
+(set-language-environment "Japanese")
+(let ((ws window-system))
+  (cond ((eq ws 'w32)
+         (prefer-coding-system 'utf-8-unix)
+         (set-default-coding-systems 'utf-8-unix)
+         (setq file-name-coding-system 'sjis)
+         (setq locale-coding-system 'utf-8))
+        ((eq ws 'ns)
+         (require 'ucs-normalize)
+         (prefer-coding-system 'utf-8-hfs)
+         (setq file-name-coding-system 'utf-8-hfs)
+         (setq locale-coding-system 'utf-8-hfs))))
+
+;; Windowsで英数と日本語にMeiryoを指定
+;; Macで英数と日本語にRictyを指定
+(let ((ws window-system))
+  (cond ((eq ws 'w32)
+         (set-face-attribute 'default nil
+                             :family "Meiryo"  ;; 英数
+                             :height 100)
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo")))  ;; 日本語
+        ((eq ws 'ns)
+         (set-face-attribute 'default nil
+                             :family "Ricty"  ;; 英数
+                             :height 140)
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty")))))  ;; 日本語
+
+;; スタートアップ非表示
+(setq inhibit-startup-screen t)
+
+;; scratchの初期メッセージ消去
+(setq initial-scratch-message "")
+
+;; ツールバー非表示
+(tool-bar-mode -1)
+
+;; メニューバーを非表示
+;;(menu-bar-mode -1)
+
+;; スクロールバー非表示
+(set-scroll-bar-mode nil)
+
+;; タイトルバーにファイルのフルパス表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name)))
+
+;; 行番号表示
+(global-linum-mode t)
+(set-face-attribute 'linum nil
+                    :foreground "#800"
+                    :height 0.9)
+
+;; 行番号フォーマット
+;;(setq linum-format "%4d")
+
+;; 括弧の範囲内を強調表示
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
+
+;; 括弧の範囲色
+(set-face-background 'show-paren-match-face "#500")
+
+;; 選択領域の色
+(set-face-background 'region "#555")
+
+;; 行末の空白を強調表示
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "#b14770")
+
+;; タブをスペースで扱う
+(setq-default indent-tabs-mode nil)
+
+;; タブ幅
+(custom-set-variables '(tab-width 4))
+
+;; yes or noをy or n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; バックアップを残さない
+(setq make-backup-files nil)
+
+;; 行間
+(setq-default line-spacing 0)
+
+;; 1行ずつスクロール
+(setq scroll-conservatively 35
+      scroll-margin 0
+      scroll-step 1)
+(setq comint-scroll-show-maximum-output t) ;; shell-mode
+
+;; フレームの透明度
+(set-frame-parameter (selected-frame) 'alpha '(0.85))
+
+;; ------------------------------------------------------------------------
+;; @ auto-install.el
+
+;; パッケージのインストールを自動化
+;; http://www.emacswiki.org/emacs/auto-install.el
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+
+;; ------------------------------------------------------------------------
+;; @ auto-complete.el
+
+;; 自動補完機能
+;; https://github.com/m2ym/auto-complete
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+  (setq ac-ignore-case t)
+  (ac-config-default))
+
+;; ------------------------------------------------------------------------
+;; @  color-theme.el
+
+;; Emacsのカラーテーマ
+;; http://code.google.com/p/gnuemacscolorthemetest/
+(when (and (require 'color-theme nil t) (window-system))
+  (color-theme-initialize)
+  (color-theme-matrix))
+
+;; ------------------------------------------------------------------------
+;; @ tabbar.el
+
+;; タブ化
+;; http://www.emacswiki.org/emacs/tabbar.el
+;;(require 'cl)
+(require 'tabbar nil t)
+
+;; scratch buffer以外をまとめてタブに表示する
+(setq tabbar-buffer-groups-function
+      (lambda (b) (list "All Buffers")))
+(setq tabbar-buffer-list-function
+      (lambda ()
+        (remove-if
+         (lambda(buffer)
+           (unless (string-match (buffer-name buffer)
+                                 "\\(*scratch*\\|*Apropos*\\|*shell*\\|*eshell*\\|*Customize*\\)")
+             (find (aref (buffer-name buffer) 0) " *"))
+           )
+         (buffer-list))))
+
+;; tabbarを有効にする
+(tabbar-mode 1)
+
+;; ボタンをシンプルにする
+(setq tabbar-home-button-enabled "")
+(setq tabbar-scroll-right-button-enabled "")
+(setq tabbar-scroll-left-button-enabled "")
+(setq tabbar-scroll-right-button-disabled "")
+(setq tabbar-scroll-left-button-disabled "")
+
+;; Ctrl-Tab, Ctrl-Shift-Tab でタブを切り替える
+(dolist (func '(tabbar-mode tabbar-forward-tab tabbar-forward-group tabbar-backward-tab tabbar-backward-group))
+  (autoload func "tabbar" "Tabs at the top of buffers and easy control-tab navigation"))
+(defmacro defun-prefix-alt (name on-no-prefix on-prefix &optional do-always)
+  `(defun ,name (arg)
+     (interactive "P")
+     ,do-always
+     (if (equal nil arg)
+         ,on-no-prefix
+       ,on-prefix)))
+(defun-prefix-alt shk-tabbar-next (tabbar-forward-tab) (tabbar-forward-group) (tabbar-mode 1))
+(defun-prefix-alt shk-tabbar-prev (tabbar-backward-tab) (tabbar-backward-group) (tabbar-mode 1))
+(global-set-key [(control tab)] 'shk-tabbar-next)
+(global-set-key [(control shift tab)] 'shk-tabbar-prev)
+
+;; GUIで直接ファイルを開いた場合フレームを作成しない
+(add-hook 'before-make-frame-hook
+          (lambda ()
+            (when (eq tabbar-mode t)
+              (switch-to-buffer (buffer-name))
+              (delete-this-frame))))
+
+;; ------------------------------------------------------------------------
+;; @  dired.el
+
+;; A simple directory explorer
+;; https://github.com/m2ym/direx-el
+(require 'direx)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
+
+;; ------------------------------------------------------------------------
+;; @ redo+.el
+
+;; redoできるようにする
+;; http://www.emacswiki.org/emacs/redo+.el
+(when (require 'redo+ nil t)
+  (define-key global-map (kbd "C-_") 'redo))
+
+;; ruby-mode
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(setq ruby-insert-encoding-magic-comment nil)
+
+;; ------------------------------------------------------------------------
+;; @  ruby-electric.el
+
+;; a minor mode that makes writing and editing Ruby code easier.
+;; https://github.com/qoobaa/ruby-electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+
+;; ------------------------------------------------------------------------
+;; @  package.el
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; ------------------------------------------------------------------------
+;; @  web-mode.el
+
+;; web-mode.el is an autonomous emacs major-mode for editing web templates
+;; http://web-mode.org/
+(require 'web-mode)
+;;; 適用する拡張子
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+;;; インデント数
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   2)
+  (setq web-mode-css-offset    2)
+  (setq web-mode-script-offset 2)
+  (setq web-mode-php-offset    2)
+  (setq web-mode-java-offset   2)
+  (setq web-mode-asp-offset    2))
+(add-hook 'web-mode-hook 'web-mode-hook)
