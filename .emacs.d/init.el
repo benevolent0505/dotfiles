cb2bb45ebcbd00866c0793dcde5e34f91f4361e5
benevolent0505 <mikio.fjt@gmail.com>
2014-10-22 02:42:51 +0900

    yatex¤òÄɲÃ

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -162,11 +162,33 @@
 (load-theme 'deeper-blue t)
 
 ;; ------------------------------------------------------------------------
+;; @ yatex
+
+;; Emacs DE TeX
+;; http://www.yatex.org/
+;; 拡張子が .tex なら yatex-mode に
+(setq auto-mode-alist
+  (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+
+;; YaTeX が利用する内部コマンドを定義する
+(setq tex-command "platex2pdf") ;; 自作したコマンドを
+(cond
+  ((eq system-type 'gnu/linux) ;; GNU/Linux なら
+    (setq dvi2-command "evince")) ;; evince で PDF を閲覧
+  ((eq system-type 'darwin) ;; Mac なら
+    (setq dvi2-command "open -a Preview"))) ;; プレビューで
+(add-hook 'yatex-mode-hook '(lambda () (setq auto-fill-function nil)))
+
+;; ------------------------------------------------------------------------
 ;; @ twittering-mode
 
 ;; Emacs DE Twitter
 ;; https://github.com/hayamiz/twittering-mode/tree/master
 (require 'twittering-mode)
+(setq twittering-use-master-password t)
+(setq twittering-icon-mode t)
+
 
 ;; ------------------------------------------------------------------------
 ;; @ egg.el
