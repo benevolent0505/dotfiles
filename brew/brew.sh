e565ee8c7959cec28084c47f73e12f8701f9aa44
benevolent0505 <mikio.fjt@gmail.com>
2014-10-25 22:39:27 +0900

    add brewfiles

---
+++ brew/brew.sh
@@ -0,0 +1,34 @@
+brew tap phinze/homebrew-cask
+
+brew install brew-cask
+brew install git
+brew install zsh --disable-etcdir
+brew install tree
+brew install openssl
+brew install readline
+brew install wget
+brew install curl
+brew install sl
+
+brew install sqlite
+brew install redis
+
+brew install node
+brew install npm
+brew install clisp
+brew install heroku-toolbelt
+
+brew cask install alfred
+brew cask install appcleaner
+brew cask install android-studio
+brew cask install coteditor
+brew cask install dropbox
+brew cask install intellij-idea-ce
+brew cask install iterm2
+brew cask install libreoffice
+brew cask install limechat
+brew cask install skype
+brew cask install virtualbox
+brew cask install vlc
+brew cask install xquartz
+brew cask install yorufukurou
---
+++ brew/brewfile.sh
@@ -0,0 +1,41 @@
+brew tap phinze/homebrew-cask
+brew tap sanemat/font
+
+brew install brew-cask
+brew install git
+brew install zsh --disable-etcdir
+brew install tree
+brew install openssl
+brew install readline
+brew install wget
+brew install curl
+brew install cmake
+brew install ricty
+brew install automake
+brew install sl
+
+brew install sqlite
+brew install mysql55
+brew install redis
+
+brew install node
+brew install npm
+brew install clisp
+brew install heroku-toolbelt
+
+brew cask install alfred
+brew cask install appcleaner
+brew cask install android-studio
+brew cask install coteditor
+brew cask install dropbox
+brew cask install evernote
+brew cask install intellij-idea-ce
+brew cask install iterm2
+brew cask install libreoffice
+brew cask install limechat
+brew cask install mactex
+brew cask install skype
+brew cask install virtualbox
+brew cask install vlc
+brew cask install xquartz
+brew cask install yorufukurou
