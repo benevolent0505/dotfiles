4f5fb966c327a2c9c22e532b38069402e662c597
benevolent0505 <mikio.fjt@gmail.com>
2014-11-19 12:33:53 +0900

    edit brew.sh

--- brew/brew.sh
+++ brew/brew.sh
@@ -4,7 +4,7 @@ brew tap peco/peco
 
 brew install brew-cask
 brew install git
-brew install zsh --disable-etcdir
+brew install zsh
 brew install tree
 brew install openssl
 brew install readline
@@ -13,27 +13,26 @@ brew install curl
 brew install sl
 brew install peco
 brew install hub
-brew install ricty
 
 brew install sqlite
 brew install redis
 brew install postgresql
 
+brew install ruby-build
+brew install rbenv
 brew install nvm
 brew install clisp
-brew install rbenv
-brew install ruby-build
 brew install heroku-toolbelt
-
 
 brew cask install alfred
 brew cask install appcleaner
 brew cask install android-studio
 brew cask install coteditor
 brew cask install dropbox
+brew cask install gyazo
+brew cask install kobito
 brew cask install intellij-idea-ce
 brew cask install iterm2
-brew cask install libreoffice
 brew cask install limechat
 brew cask install skype
 brew cask install virtualbox
