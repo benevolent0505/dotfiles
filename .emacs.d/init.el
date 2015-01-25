e4842cad3c0024cf7f1c15113ae19405317e3614
benevolent0505 <mikio.fjt@gmail.com>
2015-01-25 09:12:25 +0900

    Add yatex setting to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
 
 ;; load-pathに追加するフォルダ
 ;; 2つ以上フォルダを指定する場合の引数 => (add-to-load-path "elisp" "xxx" "xxx")
-(add-to-load-path "elisp")
+(add-to-load-path "elisp" "public_repo")
 
 ;; ------------------------------------------------------------------------
 ;; @ Cask
@@ -169,6 +169,25 @@
 
 ;; 80文字での自動改行をoff
 (setq text-mode-hook 'turn-off-auto-fill)
+
+;; ------------------------------------------------------------------------
+;; @ yatex
+
+;; Emacs DE TeX
+;; http://www.yatex.org/
+;; 拡張子が .tex なら yatex-mode に
+(setq auto-mode-alist
+  (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+
+;; YaTeX が利用する内部コマンドを定義する
+(setq tex-command "platex2pdf") ;; 自作したコマンドを
+(cond
+  ((eq system-type 'gnu/linux) ;; GNU/Linux なら
+    (setq dvi2-command "evince")) ;; evince で PDF を閲覧
+  ((eq system-type 'darwin) ;; Mac なら
+    (setq dvi2-command "open -a Preview"))) ;; プレビューで
+(add-hook 'yatex-mode-hook '(lambda () (setq auto-fill-function nil)))
 
 ;; ------------------------------------------------------------------------
 ;; @ flycheck
