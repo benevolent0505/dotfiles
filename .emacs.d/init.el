7d77bb48d266887a25441bfcc9336532a4930b7e
benevolent0505 <mikio.fjt@gmail.com>
2015-06-20 08:35:48 +0900

    git-gutterを追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -254,3 +254,8 @@
 (require 'powerline)
 (powerline-default-theme)
 
+;; ------------------------------------------------------------------------
+;; @ git-gutter
+;; Emacs port of GitGutter which is Sublime Text Plugin
+;; https://github.com/syohex/emacs-git-gutter
+(global-git-gutter-mode +1)
