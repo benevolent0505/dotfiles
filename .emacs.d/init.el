be8b78214382494577271a4c48087d0a11ef2b6e
benevolent0505 <mikio.fjt@gmail.com>
2016-05-22 18:34:01 +0900

    AquaSKKの設定を追加した

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -157,11 +157,27 @@
 (unless (server-running-p)
   (server-start))
 
+;; AquaSKK setting
+(if (eq system-type 'darwin)
+    (setq skk-server-portnum 1178))
+(if (eq system-type 'darwin)
+    (setq skk-server-host "localhost"))
+
 ;; ddskk
 (setq-default skk-user-directory "~/.ddskk")
 (el-get-bundle ddskk
   (global-set-key (kbd "C-j") 'skk-mode))
+
 (setq-default dired-bind-jump nil)
+(add-hook 'isearch-mode-hook
+          (function (lambda ()
+                      (and (boundp 'skk-mode) skk-mode
+                           (skk-isearch-mode-setup)))))
+(add-hook 'isearch-mode-end-hook
+          (function (lambda ()
+                      (and (boundp 'skk-mode) skk-mode (skk-isearch-mode-cleanup))
+                      (and (boundp 'skk-mode-invoked) skk-mode-invoked
+                           (skk-set-cursor-properly)))))
 
 ;; ------------------------------------------------------------------------
 ;; @exec-path-from-shell
