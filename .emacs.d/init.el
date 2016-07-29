7a0e958be8b0778b3ee0cf3c598694f9ab51eb1b
benevolent0505 <mikio.fjt@gmail.com>
2016-07-29 15:07:39 +0900

    Perlについての設定を追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -374,3 +374,40 @@
 
 (define-key global-map
   "\C-cG" 'scheme-other-window)
+
+;; For Perl
+;; Reference from https://github.com/shibayu36/emacs/blob/master/emacs.d/inits/20-edit-mode-perl.el
+(defalias 'perl-mode 'cperl-mode)
+(add-hook 'cperl-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-h f") 'cperl-perldoc)))
+(setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.cgi$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.psgi$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("cpanfile$" . cperl-mode) auto-mode-alist))
+
+(setq cperl-indent-level 4
+      cperl-continued-statement-offset 4
+      cperl-close-paren-offset -4
+      cperl-comment-column 40
+      cperl-highlight-variables-indiscriminately t
+      cperl-indent-parens-as-block t
+      ;; cperl-indent-subs-specially nil
+      cperl-label-offset -4
+      cperl-tab-always-indent nil
+      cperl-font-lock t)
+(add-hook 'cperl-mode-hook 'flymake-perl-load)
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (set-face-bold-p 'cperl-array-face nil)
+            (set-face-background 'cperl-array-face "black")
+            (set-face-bold-p 'cperl-hash-face nil)
+            (set-face-italic-p 'cperl-hash-face nil)
+            (set-face-background 'cperl-hash-face "black")))
+
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (progn
+               (setq indent-tabs-mode nil)
+               (setq tab-width nil))))
+
