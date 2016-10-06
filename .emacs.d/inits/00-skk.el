7dabe161034fa09193c3cddcc9db9dafd80fdc8c
benevolent0505 <mikio.fjt@gmail.com>
2016-10-07 08:55:47 +0900

    文書系バッファを開いたときは自動でskk-modeになるようにした

--- .emacs.d/inits/00-skk.el
+++ .emacs.d/inits/00-skk.el
@@ -6,8 +6,19 @@
 
 ;; ddskk
 (setq-default skk-user-directory "~/.ddskk")
+
 (el-get-bundle ddskk)
 (global-set-key (kbd "C-j") 'skk-mode)
+
+;; 文章系のバッファを開いた時には自動的に英数モード(「SKK」モード)に入る
+(let ((function #'(lambda ()
+		    (require 'skk)
+		    (skk-latin-mode-on))))
+  (dolist (hook '(find-file-hooks
+		  ;; ...
+		  mail-setup-hook
+		  message-setup-hook))
+    (add-hook hook function)))
 
 (setq-default dired-bind-jump nil)
 (add-hook 'isearch-mode-hook
