f2afbb42ffc6d1da8ce072a94d76265001b4e42d
benevolent0505 <mikio.fjt@gmail.com>
2016-10-07 09:12:45 +0900

    exec-path-from-shellがel-get-bundleでインストールされるようにした

--- .emacs.d/inits/20-exec-path-from-shell.el
+++ .emacs.d/inits/20-exec-path-from-shell.el
@@ -2,5 +2,5 @@
 ;; @exec-path-from-shell
 ;; Make Emacs use the $PATH set up by the user's shell
 ;; https://github.com/purcell/exec-path-from-shell
-(el-get-bundle exec-path-from-shell
-  (exec-path-from-shell-initialize))
+(el-get-bundle exec-path-from-shell)
+(exec-path-from-shell-initialize)
