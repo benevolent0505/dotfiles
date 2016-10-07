f0098a12645e8d74104b9a00bfe54557167898c4
benevolent0505 <mikio.fjt@gmail.com>
2016-10-08 08:22:46 +0900

    fly-checkをel-get-bundleで入るようにした

--- .emacs.d/inits/30-flycheck.el
+++ .emacs.d/inits/30-flycheck.el
@@ -2,5 +2,5 @@
 ;; @ flycheck
 ;; modern on-the-fly syntax checking extension.
 ;; https://github.com/flycheck/flycheck
-(el-get-bundle flycheck
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+(el-get-bundle flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
