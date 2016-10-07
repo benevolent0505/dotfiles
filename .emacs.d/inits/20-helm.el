94f2cc12f210bce10881beabf75eeae3420d3269
benevolent0505 <mikio.fjt@gmail.com>
2016-10-07 09:25:02 +0900

    helmをel-get-bundleでインストールできるようにした

--- .emacs.d/inits/20-helm.el
+++ .emacs.d/inits/20-helm.el
@@ -1,13 +1,15 @@
-(el-get-bundle helm
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  (helm-mode 1)
-  (helm-autoresize-mode 1)
-  ;; For find-file etc.
-  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-  ;; For helm-find-files etc.
-  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
-  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
-  (define-key global-map (kbd "C-x b")   'helm-buffers-list))
+(el-get-bundle helm)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(helm-mode 1)
+(helm-autoresize-mode 1)
+;; For find-file etc.
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x C-r") 'helm-recentf)
+(define-key global-map (kbd "C-x b")   'helm-buffers-list)
+
 ;; helm-ls-git
 (el-get-bundle! helm-ls-git)
