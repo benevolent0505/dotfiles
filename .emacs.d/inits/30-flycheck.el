42d8cf92ff599c56a3d71fb8540b32e6ac61eed4
benevolent0505 <mikio.fjt@gmail.com>
2016-10-08 08:39:20 +0900

    elispのドキュメントコメントの構文エラーを出さないようにした

--- .emacs.d/inits/30-flycheck.el
+++ .emacs.d/inits/30-flycheck.el
@@ -3,4 +3,6 @@
 ;; modern on-the-fly syntax checking extension.
 ;; https://github.com/flycheck/flycheck
 (el-get-bundle flycheck)
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
