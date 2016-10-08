c56a27bd32dec9517e461a2d9a6012b12b4b0a79
benevolent0505 <mikio.fjt@gmail.com>
2016-10-08 09:34:39 +0900

    git-messengerを導入した

---
+++ .emacs.d/inits/30-git-messenger.el
@@ -0,0 +1,8 @@
+(el-get-bundle! git-messenger)
+
+(global-set-key (kbd "C-x v p") 'git-messenger:popup-message)
+
+(define-key git-messenger-map (kbd "m") 'git-messenger:copy-message)
+
+;; Enable magit-commit-mode after typing 's', 'S', 'd'
+(add-hook 'git-messenger:popup-buffer-hook 'magit-commit-mode)
