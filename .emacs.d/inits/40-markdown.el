4c6b0f8738d46bab3d3c6619ba6805e4f6a2e480
benevolent0505 <mikio.fjt@gmail.com>
2016-10-11 21:25:22 +0900

    markdown-modeが入るようにした

--- .emacs.d/inits/40-markdown.el
+++ .emacs.d/inits/40-markdown.el
@@ -1,5 +1,5 @@
 ;; For Markdown
-(el-get-bundle markdown-mode
-  (require 'markdown-mode)
-  (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-  (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode)))
+(el-get-bundle! markdown-mode)
+
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
