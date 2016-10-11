178e558ff4d290281bc37603f1b4738f59aee1e6
benevolent0505 <mikio.fjt@gmail.com>
2016-10-11 17:10:46 +0900

    perlのテストを実行するrun-perl-test関数を書いた

--- .emacs.d/inits/40-perl.el
+++ .emacs.d/inits/40-perl.el
@@ -25,19 +25,28 @@
                (setq tab-width nil))))
 
 ;; flycheck settings
-(flycheck-define-checker perl-project-libs
-  "A perl syntac checker."
-  :command ("perl"
-            "-MProject::Libs lib_dirs => [qw(local/lib/perl5)]"
-            "-wc"
-            source-inplace)
-  :error-patterns ((error line-start
-                          (minimal-match (message))
-                          " at " (file-name) " line " line
-                          (or "." (and "," (zero-or-more not-newline)))
-                          line-end))
-  :modes (cperl-mode))
-(add-hook 'cperl-mode-hook
-          (lambda ()
-            (flycheck-mode t)
-            (setq flycheck-checker 'perl-project-libs)))
+
+;; disable my module error
+;; http://m0t0k1ch1st0ry.com/blog/2014/07/07/flycheck/
+;; (flycheck-define-checker perl-project-libs
+;;   "A perl syntax checker."
+;;   :command ("perl"
+;;             "-MProject::Libs lib_dirs => [qw(local/lib/perl5)]"
+;;             "-wc"
+;;             source-inplace)
+;;   :error-patterns ((error line-start
+;;                           (minimal-match (message))
+;;                           " at " (file-name) " line " line
+;;                           (or "." (and ", " (zero-or-more not-newline)))
+;;                           line-end))
+;;   :modes (cperl-mode))
+;; (add-hook 'cperl-mode-hook
+;;           (lambda ()
+;;             (flycheck-mode t)
+;;             (setq flycheck-checker 'perl-project-libs)))
+
+;; 要carton
+(defun run-perl-test ()
+  (interactive)
+  (compile (format "cd %s; carton exec -- prove -lvr %s" (vc-git-root default-directory) (buffer-file-name (current-buffer)))))
+
