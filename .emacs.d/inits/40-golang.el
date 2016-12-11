70024c6c043fefd9564200dac471677a34420b88
benevolent0505 <mikio.fjt@gmail.com>
2016-12-11 18:13:32 +0900

    Goの設定を追加

---
+++ .emacs.d/inits/40-golang.el
@@ -0,0 +1,9 @@
+(el-get-bundle! go-mode-autoloads in go-mode)
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(el-get-bundle! go-autocomplete)
+(el-get-bundle! go-eldoc)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+(set-face-attribute 'eldoc-highlight-function-argument nil
+                    :underline t :foreground "green"
+                    :weight 'bold)
