4cc33c74dca16e9a58d61ce1f614fc2e8eab0954
benevolent0505 <mikio.fjt@gmail.com>
2016-12-21 08:30:52 +0900

    change Mac font setting

--- .emacs.d/inits/00-languages.el
+++ .emacs.d/inits/00-languages.el
@@ -12,9 +12,9 @@
          (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Meiryo"))) ;; 日本語
         ((eq ws 'ns)
          (set-face-attribute 'default nil
-                             :family "Ricty" ;;英数
-                             :height 140)
-         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty")))
+                             :family "源ノ角ゴシック Code JP" ;;英数
+                             :height 120)
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "源ノ角ゴシック Code JP")))
         ((eq ws 'x)
          (set-face-attribute 'default nil
                              :family "Ricty"
