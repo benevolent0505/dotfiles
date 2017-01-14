2215f2a47c679286a307102295e978367632be47
benevolent0505 <mikio.fjt@gmail.com>
2017-01-14 21:38:54 +0900

    add xremap config

---
+++ config/linux/keybindings.rb
@@ -0,0 +1,20 @@
+window class_not: ['emacs-25_1', 'x-terminal-emulator'] do
+  remap 'C-b', to: 'Left'
+  remap 'C-f', to: 'Right'
+  remap 'C-p', to: 'Up'
+  remap 'C-n', to: 'Down'
+
+  remap 'M-b', to: 'Ctrl-Left'
+  remap 'M-f', to: 'Ctrl-Right'
+
+  remap 'C-a', to: 'Home'
+  remap 'C-e', to: 'End'
+
+  remap 'C-k', to: ['Shift-End', 'Ctrl-x']
+
+  remap 'C-s', to: 'C-f'
+
+  remap 'C-d', to: 'Delete'
+  remap 'C-h', to: 'BackSpace'
+  remap 'M-d', to: 'Ctrl-Delete'
+end
