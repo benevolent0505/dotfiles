5d1a2586027d91632df2314084fbe5716d84c8cc
benevolent0505 <mikio.fjt@gmail.com>
2017-04-18 09:49:44 +0900

    Temporary disable flycheck markdown-mdl

--- .emacs.d/inits/40-markdown.el
+++ .emacs.d/inits/40-markdown.el
@@ -3,3 +3,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;; markdown-mdlのlintを取り敢えずoff
+(add-hook 'markdown-mode-hook
+          '(lambda ()
+             (setq flycheck-disabled-checkers '(markdown-mdl))
+             (flycheck-mode 1)))
+
