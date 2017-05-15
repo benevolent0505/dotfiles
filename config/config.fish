0f03662c38ed5c2e8b6ad1873d4d6bd302489ebc
Unknown <mikio.fjt@gmail.com>
2017-05-15 14:10:22 +0900

    modify config.fish

--- config/config.fish
+++ config/config.fish
@@ -1,30 +1,31 @@
 # aliases
-alias ks "ls"
 alias f "open ."
-alias g "git"
 alias emacs "emacsclient -nw"
 
-alias be "bundle exec"
+if test (uname) = "Linux"
+    alias open "xdg-open"
+end
+
+# path
+set -x PATH $PATH $HOME/local/bin
 
 # rbenv
-set PATH $PATH $HOME/.rbenv/bin/
-set PATH $HOME/.rbenv/shims $PATH
+set -x PATH $PATH $HOME/.rbenv/bin/
+set -x PATH $HOME/.rbenv/shims $PATH
+rbenv init - | source
 
 # pyenv
-set PYENV_ROOT $HOME/.pyenv
-set PATH $PYENV_ROOT/bin $PATH
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
 
 # plenv
 set -x PATH $HOME/.plenv/bin $PATH
 plenv init - | source
 
-# local bin
-set PATH $PATH $HOME/local/bin/
-
 # golang
-set PATH $PATH /usr/local/go/bin
-set GOPATH $HOME/develop
-set PATH $PATH $GOPATH/bin
+set -x PATH $PATH /usr/local/go/bin
+set -x GOPATH $HOME/develop
+set -x PATH $PATH $GOPATH/bin
 
 function peco_change_directory
   ghq list -p | peco | read dir
