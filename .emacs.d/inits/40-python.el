fb48fabf6ede152849107f3ffb41ab10ae168aed
benevolent0505 <mikio.fjt@gmail.com>
2017-06-25 18:02:15 +0900

    tabでcompany-modeが開始されるようにした

--- .emacs.d/inits/40-python.el
+++ .emacs.d/inits/40-python.el
@@ -10,4 +10,5 @@
     '(lambda ()
        (auto-complete-mode -1)
        (define-key company-active-map (kbd "C-n") 'company-select-next)
-       (define-key company-active-map (kbd "C-p") 'company-select-previous))))
+       (define-key company-active-map (kbd "C-p") 'company-select-previous)
+       (define-key company-active-map (kbd "<tab>") 'company-complete))))
