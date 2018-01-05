d8a58335d987a9ae77fabb976947adaa25122820
Unknown <mikio.fjt@gmail.com>
2018-01-05 15:58:44 +0900

    window切り替えキーバインドを削除
    他のキーバインドとよく被る

--- .emacs.d/inits/90-keybind.el
+++ .emacs.d/inits/90-keybind.el
@@ -1,12 +1,5 @@
 ;; C-mにnewline-and-indentを割り当てる.初期値はnewline
 (define-key global-map (kbd "C-m") 'newline-and-indent)
 
-;; "C-t" でウィンドウを切り替える.初期値はtranspose-chars
-(define-key global-map (kbd "C-t") 'other-window)
-
 ;; "C-h"をdeleteに
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
-
-;; window change
-(global-set-key (kbd "C-x n") 'other-window)
-(global-set-key (kbd "C-x p") (lambda () (interactive) (other-window -1)))
