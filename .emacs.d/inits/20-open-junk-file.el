b74b9444c239e28c6608382ac0dd6b90125ea234
benevolent0505 <mikio.fjt@gmail.com>
2018-02-15 21:43:01 +0900

    プロキシ環境下でも使えるように

--- .emacs.d/inits/20-open-junk-file.el
+++ .emacs.d/inits/20-open-junk-file.el
@@ -1,4 +1,5 @@
 (el-get-bundle open-junk-file
-  :type emacswiki
+  :url "https://github.com/emacsmirror/emacswiki.org/blob/master/open-junk-file.el"
+  :type emacsmirror
   (setq open-junk-file-format "~/local/tmp/%Y/%m/%Y-%m-%d-%H%M%S-")
   (global-set-key (kbd "C-c j") 'open-junk-file))
