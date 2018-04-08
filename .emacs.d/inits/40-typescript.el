88c7410a47f33188739b272c0b91e5d55ececd2d
benevolent0505 <mikio.fjt@gmail.com>
2018-04-08 21:13:47 +0900

    TypeScriptの設定を取りあえず

---
+++ .emacs.d/inits/40-typescript.el
@@ -0,0 +1,31 @@
+;; For TypeScript Settings
+
+(el-get-bundle! tide
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  (add-hook 'typescript-mode-hook
+    '(lambda ()
+       ;; stop auto-complete
+       (auto-complete-mode -1)
+
+       ;; setting company keybindings
+       (define-key company-active-map (kbd "C-n") 'company-select-next)
+       (define-key company-active-map (kbd "C-p") 'company-select-previous)
+       (define-key company-active-map (kbd "<tab>") 'company-complete))))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
